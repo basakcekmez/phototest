9010623dd2794e65a3471425ff22e3d7ac911099
mhuseyincekmez@gmail.com <mhuseyincekmez@gmail.com>
2019-08-02 11:46:07 +0300

    after npm install

--- src/main/webapp/src/components/charts/testCombobox.js
+++ src/main/webapp/src/components/charts/testCombobox.js
@@ -6,23 +6,42 @@ import * as CONSTANT from '../../constant/constants';
 
 
 class TestCombobox extends Component {
+    state={
+        getAllTest:[]
+    }
     componentDidMount() {
-        axios.get(CONSTANT.serviceUrl + "getAllTest")
-            .then(result => {
-                for (var i = 0; i < result.data.length; i++) {
-                    var testCase = result.data[i];
-                    console.log(testCase.testName);
-                }
-
-            });
+        this.getAllTest();
     }
-
-    render() {
-        return (
-            <div>
-            </div>
-        );
+    getAllTest= _ =>{
+        fetch("http://localhost:7080/phototest/service/getAllTest")
+            .then(response=>response.json())
+            .then(response=>this.setState({getAllTest:response.data}))
+            .catch(err=>console.error(err))
     }
+    rendergetAllTest=({getAllTest_id,name})=><div key={getAllTest_id}>{name}</div>
+
+/* componentDidMount() {
+axios.get(CONSTANT.serviceUrl + "getAllTest")
+.then(result => {
+for (var i = 0; i < result.data.length; i++) {
+var testCase = result.data[i];
+console.log(testCase.testName);
+}
+
+});
+}*/
+
+render() {
+const{getAllTest}=this.state;
+return (
+<div className="testCombobox">
+<select value={this.state.getAllTest}>
+{getAllTest.map(this.rendergetAllTest)}
+</select>
+</div>
+
+);
+}
 }
 
 export default TestCombobox;
