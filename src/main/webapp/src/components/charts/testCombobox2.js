1243df3dcf00071d15bca1dffc73f44b04a49197
oktay.cekmez <oktay.cekmez@i2i-systems.com>
2019-08-03 14:41:14 +0300

    new comboBox

---
+++ src/main/webapp/src/components/charts/testCombobox2.js
@@ -0,0 +1,36 @@
+
+import React, { Component } from 'react';
+import Select from 'react-select';
+import faker from "faker";
+
+
+const scaryAnimals = [
+    { label: "Alligators", value: 1 },
+    { label: "Crocodiles", value: 2 },
+    { label: "Sharks", value: 3 },
+    { label: "Small crocodiles", value: 4 },
+    { label: "Smallest crocodiles", value: 5 },
+    { label: "Snakes", value: 6 },
+];
+
+class TestComboBox2 extends Component {
+    state={
+    }
+    componentDidMount() {
+    }
+    handleChange = selectedOption => {
+        this.setState({ selectedOption });
+        console.log(`Option selected:`, selectedOption);
+    };
+
+    render() {
+        return (
+           <div>
+               <Select options={scaryAnimals}   onChange={this.handleChange}/>
+           </div>
+
+        );
+    }
+}
+
+export default TestComboBox2;
--- src/main/webapp/src/index.js
+++ src/main/webapp/src/index.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestCombobox from './components/charts/testCombobox';
+import TestComboBox2 from './components/charts/testCombobox2';
 import TpsCount from './components/charts/tpsCount';
 import SuccessCount from './components/charts/successCount';
 import 'bootstrap/dist/css/bootstrap.css';
+
+import faker from 'faker';
+
+
+
+
 ReactDOM.render(
+
     <div>
-        <TestCombobox />
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <br/><br/><br/><br/>
-                        <ul className="list-group list-group-horizontal">
-                            <a href="smapp.html" className="list-group-item list-group-item-primary">Smart App</a>
-                            <a href="remote.html" className="list-group-item list-group-item-warning">Remote Plus</a>
-                            <a href="mobile.html" className="list-group-item list-group-item-success">Mobıle App</a>
-                            <a href="dongle.html" className="list-group-item list-group-item-danger">Dongle App</a>
-                            <a href="splash.html" className="list-group-item list-group-item-warning">Splash App</a>
-                            <a href="smfw.html" className="list-group-item list-group-item-info">Smart Fw</a>
-                        </ul>
-                </div>
-            </div>
-        </div>
-        {  /*
-        <TpsCount />
-        <SuccessCount />*/
-        }
+         <TestComboBox2 />
+         <div className="container">
+             <div className="row">
+                 <div className="col-md-12">
+                     <br/><br/><br/><br/>
+                         <ul className="list-group list-group-horizontal">
+                             <a href="smapp.html" className="list-group-item list-group-item-primary">Smart App</a>
+                             <a href="remote.html" className="list-group-item list-group-item-warning">Remote Plus</a>
+                             <a href="mobile.html" className="list-group-item list-group-item-success">Mobıle App</a>
+                             <a href="dongle.html" className="list-group-item list-group-item-danger">Dongle App</a>
+                             <a href="splash.html" className="list-group-item list-group-item-warning">Splash App</a>
+                             <a href="smfw.html" className="list-group-item list-group-item-info">Smart Fw</a>
+                         </ul>
+                 </div>
+             </div>
+         </div>
+
     </div>
     ,
     document.getElementById('root')
